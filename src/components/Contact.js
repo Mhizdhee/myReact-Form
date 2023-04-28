a32a7b225f9584766e6667a221cbd8bac025e0eb
mhizdhee <olaniyandorcas91@gmail.com>
2023-04-28 19:16:52 +0100

    update

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -7,38 +7,76 @@ import Images from "../Images/contact-img.jpg";
 import "../styles/contact.css";
 
 const Contact = () => {
-  const [data, setData] = useState({
+  const [formData, setFormData] = useState({
     name: "",
     email: "",
     subject: "",
     message: "",
   });
 
-  const handleChange = (e) => {
-    const value = e.target.value;
-    setData({
-      ...data,
-      [e.target.name]: value,
-    });
+  const [errors, setErrors] = useState({});
+
+  const validateForm = () => {
+    let errors = {};
+
+    if (!formData.name) {
+      errors.name = "Name is required";
+    }
+
+    if (!formData.email) {
+      errors.email = "Email is required";
+    } else if (!/\S+@\S+\.\S+/.test(formData.email)) {
+      errors.email = "Email is invalid";
+    }
+
+    if (!formData.message) {
+      errors.message = "Message is required";
+    }
+
+    return errors;
   };
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    const userData = {
-      name: data.name,
-      email: data.email,
-      subject: data.subject,
-      message: data.message,
-    };
-    axios
-      .post(
-        "https://my-json-server.typicode.com/tundeojediran/contacts-api-server/inquiries",
-        userData
-      )
-      .then((response) => {
-        console.log(response.status, response.data.token);
-      });
+
+    const errors = validateForm();
+    setErrors(errors);
+
+    if (Object.keys(errors).length === 0) {
+      // submit form
+    }
   };
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prevFormData) => ({ ...prevFormData, [name]: value }));
+  };
+
+  // const handleChange = (e) => {
+  //   const value = e.target.value;
+  //   setData({
+  //     ...data,
+  //     [e.target.name]: value,
+  //   });
+  // };
+
+  // const handleSubmit = (e) => {
+  //   e.preventDefault();
+  //   const userData = {
+  //     name: data.name,
+  //     email: data.email,
+  //     subject: data.subject,
+  //     message: data.message,
+  //   };
+  axios
+    .post(
+      "https://my-json-server.typicode.com/tundeojediran/contacts-api-server/inquiries"
+      // userData
+    )
+    .then((response) => {
+      console.log(response.status, response.data.token);
+    });
+  // };
 
   return (
     <>
@@ -50,35 +88,45 @@ const Contact = () => {
           <div className="form-card">
             <form onSubmit={handleSubmit}>
               <Input
-                type="name"
-                value={data.name}
+                type="text"
+                name="name"
+                value={formData.name}
                 onChange={handleChange}
                 variant="filled"
                 placeholder="Name"
               />
+              {errors.name && <div className="error">{errors.name}</div>}
+
               <Input
                 type="email"
-                value={data.email}
+                name="email"
+                value={formData.email}
                 onChange={handleChange}
                 variant="filled"
                 placeholder="Email"
               />
+              {errors.email && <div className="error">{errors.email}</div>}
+
               <Input
-                type="subject"
-                value={data.subject}
+                type="text"
+                name="subject"
+                value={formData.subject}
                 onChange={handleChange}
                 variant="filled"
                 placeholder="Subject"
               />
+
               <Textarea
-                type="message"
-                value={data.message}
+                name="message"
+                value={formData.message}
                 onChange={handleChange}
                 variant="filled"
                 placeholder="Message..."
                 rows="4"
                 cols="50"
               />
+              {errors.message && <div className="error">{errors.message}</div>}
+
               <div className="btn">
                 <Button colorScheme="linkedin">Submit</Button>
               </div>
